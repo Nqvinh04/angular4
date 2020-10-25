1291a85c8422656039b57250343e72588aa0f4cf
Nqvinh04 <nqvinh1995@gmail.com>
2020-10-25 22:47:46 +0700

    Input and Output

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,22 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.scss']
+})
+export class ChildComponent implements OnInit {
+    @Output() myClick = new EventEmitter<boolean>();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+    addForParent() {
+        this.myClick.emit(true);
+    }
+
+    subForParent() {
+        this.myClick.emit(false);
+    }
+}
